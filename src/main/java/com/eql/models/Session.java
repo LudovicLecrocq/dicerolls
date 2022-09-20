6eb7c2e9fae6a111ae4e93b77e781529a20c642f
Ludovic Lecrocq <you@example.com>
2022-09-20 16:58:48 +0200

    ajout Session

--- src/main/java/com/eql/models/Session.java
+++ src/main/java/com/eql/models/Session.java
@@ -5,28 +5,24 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-import javax.persistence.Table;
-import java.util.ArrayList;
-import java.util.List;
-
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-
 @Entity
-@Table(name = "roles")
-public class Role {
+public class Session {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
-    private String label;
-    @ManyToMany(mappedBy = "roles")
-    private List<User> users = new ArrayList<>();
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    private String name;
+    private String password;
+
 }
